e2e4042c1cc17975b7f9076a3db1d12d30feafde
Jordan <jordan@Jordans-MacBook-Air.local>
2023-09-20 08:03:09 -0600

    initial commit

---
+++ src/app/strength.pipe.spec.ts
@@ -0,0 +1,34 @@
+import { StrengthPipe } from './strength.pipe';
+
+describe('StrengthPipe', () => {
+  it('create an instance', () => {
+    const pipe = new StrengthPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
+
+describe('StrengthPipe', () => {
+  it('should display weak if strength is less than 5', () => {
+    //arrange
+    const pipe = new StrengthPipe();
+    
+    //act
+    var result = pipe.transform(5);
+  
+    //assert
+    expect(result).toBeTruthy
+    expect(result).toEqual('5 (weak)');
+  });
+
+  it('should display strong if strength is 10', () => {
+    const pipe = new StrengthPipe();
+
+    expect(pipe.transform(10)).toEqual('10 (strong)');
+  });
+
+  it('should display unbelievable if strength is > 20', () => {
+    const pipe = new StrengthPipe();
+
+    expect(pipe.transform(21)).toEqual('21 (unbelievable)');
+  });
+});
